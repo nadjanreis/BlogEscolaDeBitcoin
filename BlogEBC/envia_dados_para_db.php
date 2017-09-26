40de81b063fca662350678061f3567279222dd33
nadjanreis <32284194+nadjanreis@users.noreply.github.com>
2017-09-25 23:06:38 -0300

    Add files via upload

---
+++ BlogEBC/envia_dados_para_db.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<div>
+<?php 
+
+$nome= $_POST["tnome"];
+$email= $_POST["temail"];
+
+echos "Tentando gravar seus dados no banco de dados... ";
+
+ ?>
+</div>
+</body>
+</html>
